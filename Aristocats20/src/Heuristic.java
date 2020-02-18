e2e2c183d4e49b6090983b7d0e3bfc6c32ff49c7
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-02-18 22:41:19 +0000

    [UPDATE] Added binary search to the Heuristic class.

--- Aristocats20/src/Heuristic.java
+++ Aristocats20/src/Heuristic.java
@@ -1,2 +1,45 @@
-public class Heuristic {
+import java.util.Arrays;
+
+public class Heuristic
+{
+
+    public int endIndex;
+
+    /*
+        Binary search
+     */
+    private int findEndIndex(int[] array, int target)
+    {
+        this.endIndex = Arrays.binarySearch(array, target);
+
+        if(endIndex > -1)
+        {
+            return 1; // Target found
+        }
+        else // Target not found
+        {
+            endIndex += 2; // Add two to move to current max item that is less than the target
+            endIndex *= -1; // Make the index positive
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] array = {1, 2, 4, 5, 6};
+        int[] array2 = {1, 2, 3, 4, 5, 6,};
+
+        Heuristic h = new Heuristic();
+
+        h.findEndIndex(array, 3);
+
+        System.out.println("End index: " + h.endIndex);
+        h.findEndIndex(array2, 3);
+
+        System.out.println("End index: " + h.endIndex);
+
+    }
+
+
 }
