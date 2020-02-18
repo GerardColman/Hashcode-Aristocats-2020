2d3e189abba4187fc09e69194705ed3dbf88e255
sahil-mehra <sm.sahilmehra@gmail.com>
2020-02-18 22:53:45 +0000

    Create the bones of my CheckSum method

--- Aristocats20/src/Heuristic.java
+++ Aristocats20/src/Heuristic.java
@@ -1,32 +1,11 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Heuristic
-{
-
+public class Heuristic {
+    ArrayList<Integer> arrayList; // ArrayList of indexes to be returned
     public int endIndex;
 
-    /*
-        Binary search
-     */
-    private int findEndIndex(int[] array, int target)
-    {
-        this.endIndex = Arrays.binarySearch(array, target);
-
-        if(endIndex > -1)
-        {
-            return 1; // Target found
-        }
-        else // Target not found
-        {
-            endIndex += 2; // Add two to move to current max item that is less than the target
-            endIndex *= -1; // Make the index positive
-        }
-
-        return 0;
-    }
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int[] array = {1, 2, 4, 5, 6};
         int[] array2 = {1, 2, 3, 4, 5, 6,};
 
@@ -41,5 +20,29 @@ public class Heuristic
 
     }
 
+    // This loops through the array till
+    public void CheckSum(int[] input){
+        boolean n = false;
+        while(n == false){
+            int a = endIndex; //
+
+        }
+    }
+
+    /*
+        Binary search
+     */
+    private int findEndIndex(int[] array, int target) {
+        this.endIndex = Arrays.binarySearch(array, target);
+
+        if (endIndex > -1) {
+            return 1;// Target found
+        } else { // Target not found
+            endIndex += 2; // Add two to move to current max item that is less than the target
+            endIndex *= -1; // Make the index positive
+        }
+
+        return 0;
+    }
 
 }
