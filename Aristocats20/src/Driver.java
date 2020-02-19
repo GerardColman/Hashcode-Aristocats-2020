a54f4084a3680a592783b2347f3ff11210e9b65f
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-02-19 00:15:42 +0000

    [UPDATE] Added valid output functionality.

--- Aristocats20/src/Driver.java
+++ Aristocats20/src/Driver.java
@@ -3,9 +3,37 @@
     Driver Class for HashCode Practice Pizza Problem
  */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Driver {
+
+    public static int parseData(int[] array, int target, int endIndex, ArrayList<Integer> indices)
+    {
+        int sum = 0;
+        int i = endIndex;
+        int numPizzas = 0;
+
+        while(i >= 0 && sum != target)
+        {
+            if(sum + array[i] < target)
+            {
+                sum += array[i];
+                numPizzas++;
+                indices.add(i);
+            }
+            i--;
+        }
+
+        if(sum == target)
+        {
+            //System.out.println("We did it!");
+        }
+
+        // System.out.println("Sum: " + sum);
+
+        return numPizzas;
+    }
 
     public static int[] readData(PizzaIO io, String path)
     {
@@ -13,9 +41,9 @@ public class Driver {
         {
             int[] array = io.intialise(path);
 
-            System.out.println(Arrays.toString(array));
-            System.out.println(io.maximum);
-            System.out.println(io.numOfTypes);
+           // System.out.println(Arrays.toString(array));
+           // System.out.println(io.maximum);
+           // System.out.println(io.numOfTypes);
 
             return array;
         }
@@ -27,6 +55,17 @@ public class Driver {
         return null;
     }
 
+    public static int calculateScore(ArrayList<Integer> indices, int[] array) {
+        int sum = 0;
+
+        for(int i = 0; i < indices.size(); i++)
+        {
+            sum += array[indices.get(i)];
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
 
         String[] filePaths = {"src/a_example.in", "src/b_small.in", "src/c_medium.in", "src/d_quite_big.in", "src/e_also_big.in"};
@@ -36,10 +75,25 @@ public class Driver {
             PizzaIO io = new PizzaIO();
             int[] array =  readData(io, filePaths[i]);
 
-            System.out.println("Target: " + io.maximum + ", num of types: " + io.numOfTypes);
-            System.out.println(Arrays.toString(array));
+            //System.out.println("Target: " + io.maximum + ", num of types: " + io.numOfTypes);
+            // System.out.println(Arrays.toString(array));
 
             // Apply heuristic
+            Heuristic h = new Heuristic();
+            h.findEndIndex(array, io.maximum);
+
+
+            ArrayList<Integer> indices = new ArrayList<>();
+            int count = parseData(array, io.maximum, h.endIndex, indices);
+
+            System.out.println(count);
+
+            for(int j = 0; j < indices.size(); j++)
+            {
+                System.out.print(indices.get(j) + " ");
+            }
+
+            System.out.println();
 
             // Score the result
 
