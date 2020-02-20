ccdc9c9ae0c084fcda0533ad5c5538f01c9eea6a
Gerard Colman <gerard.colman1@gmail.com>
2020-02-20 20:47:29 +0000

    Initial sorting algorithm

--- Aristocats20/src/real/LibrarySelection.java
+++ Aristocats20/src/real/LibrarySelection.java
@@ -1,7 +1,6 @@
 package real;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Comparator;
 // This is the library selection algorithm
 
 public class LibrarySelection  {
@@ -16,26 +15,44 @@ public class LibrarySelection  {
 
     public void libSort() {
         int signUpTime = 1;
-
-        lib1stSort = io.libraries;
-
-        System.out.println(io.libraries.toString());
-
-        lib1stSort.sort(new Comparator<Library>()
-        {
-            @Override
-            public int compare(Library o1, Library o2) {
-                return Integer.compare(o1.signUpTime, o2.signUpTime);
+        int pointerSelect = 0;
+        int pointerSort = 0;
+        boolean isBiggest = false;
+        /*First sorting pass*/
+        for(int days=1;days<FileIO.daysForScanning;days++) {
+            for(int i = 0;i<io.libraries.size();i++){
+                if(io.libraries.get(i).signUpTime > io.daysForScanning){
+                    i++;
+                }
+                if(io.libraries.get(i).signUpTime == days){
+                    lib1stSort.add(io.libraries.get(i));
+                }
             }
-        });
-
-        for(int days = 1; days < FileIO.daysForScanning; days++)
-        {
-            break;
         }
-
+        /*Second sorting pass*/
+        for(int i = 0;i<io.daysForScanning;i++){
+            for(pointerSelect = 0;pointerSelect<lib1stSort.size();pointerSelect++){
+                while(lib1stSort.get(pointerSelect).signUpTime == i) {
+                    if (lib1stSort.get(pointerSelect).signUpTime < lib1stSort.get(pointerSort).signUpTime) {
+                        pointerSelect = pointerSort;
+                    }
+                }
+            }
+        }
         libSignUpOrder = lib1stSort;
-
-        System.out.println(lib1stSort.toString());
+    }
+    private int getBlockCount(int blockIndex){ //returns the amount in an individual block
+        if(blockIndex == 0){
+            return 0;
+        }
+        int BlockCount = 0;
+        int pointer = 0;
+        while(lib1stSort.get(pointer).signUpTime != blockIndex){ //get to start of needed block
+            pointer++;
+        }
+        while(lib1stSort.get(pointer).signUpTime != blockIndex + 1){
+            BlockCount++;
+        }
+        return BlockCount;
     }
 }
