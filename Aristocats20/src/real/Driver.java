754d0e5659e393de6dcdd79bb1079556a25cb1c7
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-02-20 17:47:19 +0000

    [UPDATE] Added problem + data.

--- Aristocats20/src/real/Driver.java
+++ Aristocats20/src/real/Driver.java
@@ -1,4 +1,25 @@
 package real;
 
 public class Driver {
+
+    public static void main(String[] args)
+    {
+        String[] filePaths = {};
+
+        for(int i = 0; i < 6; i++)
+        {
+            // File IO
+            FileIO io = new FileIO();
+
+            // Algorithm
+
+
+
+            // Output results
+
+
+        }
+
+
+    }
 }
