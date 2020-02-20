b819ddbee2f37526ab06c7977b8f2ca0b30bd3c8
cormic2000 <43865040+cormic2000@users.noreply.github.com>
2020-02-20 17:35:06 +0000

    Update FileIO.java

--- Aristocats20/src/real/FileIO.java
+++ Aristocats20/src/real/FileIO.java
@@ -1,4 +1,38 @@
 package real;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FileIO {
+    public static void main(String[] args) throws IOException {
+        InputOutput p = new InputOutput();
+        String[] fileNames = {"a_example"};
+
+        p.getInput(fileNames[0]);
+    }
+
+    public void getInput(String fileName) throws IOException{
+
+        BufferedReader in = new BufferedReader(new FileReader("input\\" + fileName + ".in"));
+
+        String line;
+        line = in.readLine();
+        line.trim();
+
+        String[] use;
+        use = line.split(" ");
+        in.close();
+
+    }
+
+    public void printOutput(String fileName) throws FileNotFoundException {
+
+        //System.out.println("\n-------- output " + fileName);
+        PrintWriter out = new PrintWriter("output\\" + fileName + ".in");
+        out.println();
+        out.close();
+    }
+
 }
