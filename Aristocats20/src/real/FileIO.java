b5d2e74984a0c99215df9dd4a31efac473e06dfd
cormic2000 <43865040+cormic2000@users.noreply.github.com>
2020-02-20 18:35:31 +0000

    Update FileIO.java

--- Aristocats20/src/real/FileIO.java
+++ Aristocats20/src/real/FileIO.java
@@ -4,35 +4,64 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class FileIO {
-    public static void main(String[] args) throws IOException {
-        InputOutput p = new InputOutput();
-        String[] fileNames = {"a_example"};
-
-        p.getInput(fileNames[0]);
-    }
-
-    public void getInput(String fileName) throws IOException{
-
+	
+	ArrayList<Library> libraries = new ArrayList<Library>();
+	
+	public static void main(String[] args) throws IOException {
+		FileIO p = new FileIO();
+		String[] fileNames = {"a_example"};
+		
+		p.getInput(fileNames[0]);
+	}
+	
+	public void getInput(String fileName) throws IOException{
+		int sum = 0;
         BufferedReader in = new BufferedReader(new FileReader("input\\" + fileName + ".in"));
 
-        String line;
-        line = in.readLine();
-        line.trim();
+        String firstline, secondline, libraryInfo, bookInfo;
+        firstline = in.readLine();
+        secondline = in.readLine();
 
-        String[] use;
-        use = line.split(" ");
+        firstline.trim();
+        secondline.trim();
+        String[] first, second, library, book;
+
+        first = firstline.split(" ");
+        second = secondline.split(" ");
+
+        for(int count = 0;count < Integer.parseInt(first[1]);count++){
+            libraryInfo = in.readLine();
+            bookInfo = in.readLine();
+            libraryInfo.trim();
+            bookInfo.trim();
+            library = libraryInfo.split(" ");
+            book = bookInfo.split(" ");
+            
+            ArrayList<Book> bookArr = new ArrayList<Book>();
+            for(int i = 0;i<Integer.parseInt(library[0]);i++) {
+            	
+            	int temp = Integer.parseInt(book[i]);
+            	sum = sum + Integer.parseInt(second[temp]);
+            	Book n = new Book(Integer.parseInt(book[i]), Integer.parseInt(second[temp]));
+            	bookArr.add(n);
+            }
+            
+            Library a = new Library(count ,bookArr,  Integer.parseInt(library[1]), Integer.parseInt(library[2]), sum);
+            libraries.add(a);
+        }
         in.close();
 
     }
-
-    public void printOutput(String fileName) throws FileNotFoundException {
-
-        //System.out.println("\n-------- output " + fileName);
-        PrintWriter out = new PrintWriter("output\\" + fileName + ".in");
-        out.println();
-        out.close();
-    }
-
+	public void printOutput(String fileName) throws FileNotFoundException {
+		
+		//System.out.println("\n-------- output " + fileName);
+		PrintWriter out = new PrintWriter("output\\" + fileName + ".in");
+			out.println();
+			out.close();
+	}
+	
 }
+
