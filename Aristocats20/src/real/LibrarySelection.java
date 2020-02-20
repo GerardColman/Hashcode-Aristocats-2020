19cfccf888b23c89b69552222611a18fb64668a4
sahil-mehra <sm.sahilmehra@gmail.com>
2020-02-20 21:28:22 +0000

    Fixed libsort

--- Aristocats20/src/real/LibrarySelection.java
+++ Aristocats20/src/real/LibrarySelection.java
@@ -1,11 +1,14 @@
 package real;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 // This is the library selection algorithm
 
 public class LibrarySelection  {
     public ArrayList<Library> libSignUpOrder = new ArrayList<Library>(); //Libraries sorted by sign up time and by amount of points in library
     private ArrayList<Library> lib1stSort = new ArrayList<Library>(); //Libraries sorted by just sign up times
+    private ArrayList<Library> tempArray = new ArrayList<>();
     FileIO io = null;
 
     public LibrarySelection(FileIO io)
@@ -15,43 +18,28 @@ public class LibrarySelection  {
 
     public void libSort() {
         int signUpTime = 1;
-        int pointerSelect = 0;
-        int pointerSort = 0;
-        boolean isBiggest = false;
         /*First sorting pass*/
         for(int days=1;days<FileIO.daysForScanning;days++) {
+            tempArray.clear();
             for(int i = 0;i<io.libraries.size();i++){
                 if(io.libraries.get(i).signUpTime > io.daysForScanning){
                     i++;
-                }
-                if(io.libraries.get(i).signUpTime == days){
-                    lib1stSort.add(io.libraries.get(i));
+                } else if (io.libraries.get(i).signUpTime == days){
+                    tempArray.add(io.libraries.get(i));
                 }
             }
-        }
-        /*Second sorting pass*/
-        for(int i = 0;i<io.daysForScanning;i++){
-            pointerSelect = getBlockCount(i-1)+1;
-            if(i == 0){
-                pointerSelect = 0;
-            }
-            while(lib1stSort.get(pointerSelect).signUpTime == i) {
-                for (pointerSort = getBlockCount(i-1)+1; pointerSort < getBlockCount(i); pointerSort++) {
-                    if(i == 0){
-                        pointerSort = 0;
+            int j = 0;
+            while(j != tempArray.size()){
+                tempArray.sort(new Comparator<Library>()
+                {
+                    @Override
+                    public int compare(Library o1, Library o2) {
+                        return Integer.compare(o1.booksPerDay,o2.booksPerDay);
                     }
-                    if (lib1stSort.get(pointerSort).signUpTime > lib1stSort.get(pointerSelect).signUpTime) {
-                        pointerSelect = pointerSort;
-                    }else{
-                        libSignUpOrder.add(lib1stSort.get(pointerSelect));
-                        lib1stSort.remove(pointerSelect);
-                        pointerSelect = getBlockCount(i-1)+1;
-                        if(i == 0){
-                            pointerSelect = 0;
-                        }
-                    }
-                }
+                });
+                j++;
             }
+            libSignUpOrder.addAll(tempArray);
         }
     }
     private int getBlockCount(int blockIndex){ //returns the amount in an individual block
